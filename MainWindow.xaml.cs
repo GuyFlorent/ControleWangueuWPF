9b9bbe1306274efb1366a3ee7a2427410fc73c99
stagiaire <stagiaire@PC-B-6-95R9J32>
2019-10-18 17:00:02 +0200

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControleWangueuWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Client> test;
+        private Client cli;
+        private List<Produit> prod;
+        private Produit clipro;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            donnees donnees = new donnees();
+            // donnees.ajouterClient("etoo", "fils", "etoo.@yohh.com", "hyir");
+            prod = donnees.ListeProduit();
+
+            produitDataGrid.DataContext = prod;
+            test = donnees.ListeClient();
+
+            clientDataGrid.DataContext = test;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource clientViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("clientViewSource")));
+            // Charger les données en définissant la propriété CollectionViewSource.Source :
+            // clientViewSource.Source = [source de données générique]
+            System.Windows.Data.CollectionViewSource produitViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("produitViewSource")));
+            // Charger les données en définissant la propriété CollectionViewSource.Source :
+            // produitViewSource.Source = [source de données générique]
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MonTab.SelectedIndex = 2;
+            int i = clientDataGrid.SelectedIndex;
+
+            this.cli = test[i];
+            txtNom.Text = cli.nom;
+            txtPrenom.Text = cli.prenom;
+            txtEmail.Text = cli.email;
+            txtPassWord.Text = cli.password;
+           
+
+        }
+
+        private void Modifier_Click(object sender, RoutedEventArgs e)
+        {
+            donnees donnees = new donnees();
+
+            this.cli.email = txtEmail.Text;
+            this.cli.nom = txtNom.Text;
+            this.cli.prenom = txtPrenom.Text;
+            this.cli.password = txtPassWord.Text;
+
+            donnees.ModifierClient(this.cli);
+            test = donnees.ListeClient();
+
+            clientDataGrid.DataContext = test;
+            MessageBox.Show("modifier avec succes!");
+            txtNom.Text = ""; txtPrenom.Text = ""; txtEmail.Text = ""; txtPassWord.Text = "";
+
+        }
+
+        private void Ajouter_Click(object sender, RoutedEventArgs e)
+        {
+            donnees donnees = new donnees();
+            if (txtNom.Text != null && txtPrenom.Text != null && txtEmail.Text != null && txtPassWord.Text != null &&
+                txtNom.Text != "" && txtPrenom.Text != "" && txtEmail.Text != "" && txtPassWord.Text != "")
+            {
+                donnees.ajouterClient(txtNom.Text, txtPrenom.Text, txtEmail.Text, txtPassWord.Text);
+            }
+            else 
+            {
+                donnees.ajouterProduit(txtNom.Text);
+                prod = donnees.ListeProduit();
+                produitDataGrid.DataContext = prod;
+
+            }
+            
+            test = donnees.ListeClient();
+
+            clientDataGrid.DataContext = test;
+            txtNom.Text = ""; txtPrenom.Text = ""; txtEmail.Text = ""; txtPassWord.Text = "";
+        }
+
+        private void Supprimer_Click(object sender, RoutedEventArgs e)
+        {
+            donnees donnees = new donnees();
+            donnees.supprimerClient(this.cli);
+            test = donnees.ListeClient();
+
+            clientDataGrid.DataContext = test;
+            MessageBox.Show("Supprimer avec succes!");
+
+        }
+
+        private void clientDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          //  donnees donnees = new donnees();
+           // donnees.ModifierClient(this.cli);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MonTab.SelectedIndex = 1;
+            int i = clientDataGrid.SelectedIndex;
+
+            this.clipro= prod[i];
+            txtNom.Text = clipro.nom;
+        }
+    }
+}
---
+++ donnees.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleWangueuWPF
+{
+    class donnees
+    {
+        WANGUEUEntities dbContext = new WANGUEUEntities();
+
+        public void Actualiser()
+        {
+
+        }
+        public List<Client> ListeClient()
+        {
+            
+            var liste = dbContext.Clients.ToList();
+            return liste;
+        }
+
+        public void ajouterClient(string nom, string prenom, string email, string password)
+        {
+            var client = new Client();
+            client.nom = nom;
+            client.prenom = prenom;
+            client.email = email;
+            client.password = password;
+
+            dbContext.Clients.Add(client);
+            dbContext.SaveChanges();
+        }
+
+
+        public void supprimerClient(Client cli)
+        {
+            var clientSuprimer = dbContext.Clients.FirstOrDefault(f => f.id_client == cli.id_client);
+            if (clientSuprimer != null)
+            {
+                dbContext.Clients.Remove(clientSuprimer);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void ModifierClient(Client cli)
+        {
+            var clientModifier = dbContext.Clients.FirstOrDefault(f => f.id_client == cli.id_client);
+            clientModifier.nom = cli.nom;
+            clientModifier.prenom = cli.prenom;
+            clientModifier.email = cli.email;
+            clientModifier.password = cli.password;
+
+            dbContext.SaveChanges();
+
+        }
+
+
+
+        public List<Produit> ListeProduit()
+        {
+            
+            var liste = dbContext.Produits.ToList();
+            return liste;
+        }
+
+    public void ajouterProduit(string nom)
+    {
+        var produit = new Produit();
+            produit.nom = nom;
+
+            dbContext.Produits.Add(produit);
+       dbContext.SaveChanges();
+    }
+
+
+    public void supprimerCommande(string email)
+    {
+        var commandeSUprime = dbContext.Clients.FirstOrDefault(f => f.email == email).Commandes.ToList();
+        if (commandeSUprime != null)
+        {
+           // dbContext.Clients.Remove(clientSuprimer);
+            dbContext.SaveChanges();
+        }
+    }
+
+}
+}
